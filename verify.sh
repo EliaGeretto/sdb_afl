a9b9c03557347335ebf3a79acee350ecc97fea16
Elia Geretto <elia.f.geretto@gmail.com>
2017-04-14 16:46:02 +0200

    Run full test suite when minimized is fine

--- verify.sh
+++ verify.sh
@@ -6,6 +6,7 @@ NC='\033[0m'
 
 EXECUTABLE=sdb_asan
 MINIMIZED_PATH=minimized
+CRASHES_PATH=findings/crashes
 
 COUNT=0
 for CRASH in $MINIMIZED_PATH/id*
@@ -25,3 +26,26 @@ done
 
 echo
 echo "$COUNT errors found."
+
+if [ $COUNT == 0 ]
+then
+	echo 'Well done! Running full suite...'
+	echo
+	for CRASH in $CRASHES_PATH/id*
+	do
+		./$EXECUTABLE - < $CRASH &> /dev/null
+		EXIT=$?
+
+		echo -n "$CRASH: "
+		if [ $EXIT == 1 ]
+		then
+			COUNT=$((COUNT + 1))
+			echo -e "${RED}fail${NC}"
+		else
+			echo -e "${GREEN}pass${NC}"
+		fi
+	done
+
+	echo
+	echo "$COUNT errors found."
+fi
